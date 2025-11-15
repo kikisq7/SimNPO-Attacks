b141b2fa86e2e3e1cdb9a99559e014e75de68be1
Sydney Du <sydneydu@gmail.com>
2025-11-15 12:42:25 -0500

    adds constants.py

---
+++ src/set_difference_pruning/alignment_attribution_code/lib/constants.py
@@ -0,0 +1,4 @@
+import os
+MODEL_CACHE_DIR = os.environ.get("MODEL_CACHE_DIR", r"/content/SimNPO-Attacks/model_cache")
+RESULTS_DIR = os.environ.get("RESULTS_DIR", r"/content/SimNPO-Attacks/results")
+EVAL_CACHE_PATH = os.environ.get("EVAL_CACHE_PATH", r"/content/SimNPO-Attacks/eval_cache")
